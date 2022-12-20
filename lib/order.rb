0aa40a38ed25da082e9820e9d885a9a6a49b2b1c
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-20 19:57:47 +0000

    Integration test for add and read_back

--- lib/order.rb
+++ lib/order.rb
@@ -10,6 +10,16 @@ class Order
   end
 
   def read_back
-    @user_order
+    @user_order.empty? ? "Your order is currently empty. Please choose some items" : @user_order
+  end
+
+  def print_receipt
+    # returns an itemised receipt from instance var
+    # Only prints receipt if order is placed
+  end
+
+  def place_order
+    # when this is called automated text is sent
+    # sets order placed to true
   end
 end
