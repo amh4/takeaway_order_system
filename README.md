b5ae6da891b2a41575e4e7d54f5596aae5d74c5d
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-22 15:28:48 +0000

    Added README

---
+++ README.md
@@ -0,0 +1,9 @@
+# Ruby Takeaway Ordering System
+
+## Description
+
+This is a Ruby only multiclass program that takes a user's choice of items, verifies they are on the menu, allows the order to be read back prior to ordering, then when the order is placed the user gets an automated text via the Twilio API confirming pickup time, finally they can request an itemised receipt with a grand total cost.
+
+### Next steps
+
+Future considerations would be to present the menu and collect user choices through a UI rather than via the add method.
--- lib/texting.rb
+++ lib/texting.rb
@@ -30,7 +30,7 @@ class Text
     account_sid = ENV["ssid"]
     auth_token = ENV['auth_token']
     client = Twilio::REST::Client.new(account_sid, auth_token)
-
+    collection_time = Time.now + 45*60*0
     cleaned_number = phone_number.split("").drop(1).unshift("+44").join("")
 
     from = +18106283576
@@ -39,7 +39,7 @@ class Text
     client.messages.create(
       from: from,
       to: to,
-      body: "Thank you #{name}. Your order has been placed and will be ready at #{Time.now + 45*60*0}"
+      body: "Thank you #{name}. Your order has been placed and will be ready at #{collection_time}"
     )
     return "Message sent successfully"
   end
