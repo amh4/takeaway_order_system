9b5805c8b190b46f49644a6118ee0d08839288a6
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-22 14:18:13 +0000

    Mock tested read_back and add complete

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -9,10 +9,32 @@ RSpec.describe Order do
     expect(my_order.read_back).to eq [{"Egg Fried Rice" => 4.00}]
   end
   
-  it "Takes an instance of menu (item), adds it to order, reads it back" do
+  it "If no item is added it returns notice" do
     my_order = Order.new("Adam", "07584573146")
     fake_choice = double :fake_choice
-    allow(fake_choice).to receive(:list_choice).and_return("Your order is empty")
     expect(my_order.read_back).to eq "Your order is empty"
   end
+  
+  it "Takes an instance of menu (item), and notifies user if not on menu" do
+    my_order = Order.new("Adam", "07584573146")
+    fake_choice = double :fake_choice
+    allow(fake_choice).to receive(:list_choice).and_return("That is not on the menu")
+    expect(my_order.add(fake_choice)).to eq "That is not on the menu"
+  end
+
+  it "Takes an instance of menu (item), and notifies user if not on menu" do
+    my_order = Order.new("Adam", "07584573146")
+    fake_choice = double :fake_choice
+    allow(fake_choice).to receive(:list_choice).and_return("That is not on the menu")
+    expect(my_order.add(fake_choice)).to eq "That is not on the menu"
+  end
+
+  it "Place order is selected without adding item, notify user" do
+    my_order = Order.new("Adam", "07584573146")
+    fake_choice = double :fake_choice
+    expect(my_order.place_order).to eq "You have not added any items to your order"
+  end
+  
+    
+  
 end
