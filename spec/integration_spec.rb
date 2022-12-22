75ef2eac658bb381ffdbe5b54568f0c9fc308c4d
Adam Hoar <42139484+amh4@users.noreply.github.com>
2022-12-22 15:16:28 +0000

    Updated mob number from ENV

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,12 +1,14 @@
 require_relative "../lib/menu"
 require_relative "../lib/order"
 require_relative "../lib/texting"
+require 'dotenv'
+Dotenv.load('file1.env')
 
 RSpec.describe Order do
   it "adds item to order and reads back the order" do
     choice_1 = Menu.new("Egg Fried Rice")
     choice_2 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     my_order.add(choice_1)
     my_order.add(choice_2)
     expect(my_order.read_back).to eq [{"Egg Fried Rice"=>4.0}, {"Prawn Crackers"=>2.0}]
@@ -14,21 +16,21 @@ RSpec.describe Order do
   
   it "adds item to order and reads back the order" do
     choice_1 = Menu.new("Egg Fried Rice")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     expect(my_order.read_back).to eq "Your order is empty"
     
   end
   
   it "returns an message stating the item is not on the menu" do
     choice_1 = Menu.new("Chicken Nuggets")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     expect(my_order.add(choice_1)).to eq "That is not on the menu"
   end 
   
   it "if order is empty and they ask to place returns message to inform user" do
     choice_1 = Menu.new("Egg Fried Rice")
     choice_2 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     order_message = Text.new(my_order)
     expect(my_order.place_order).to eq "You have not added any items to your order"
   end
@@ -36,7 +38,7 @@ RSpec.describe Order do
   it "if order is less than 6 items text says 30 mins" do
     choice_1 = Menu.new("Egg Fried Rice")
     choice_2 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     order_message = Text.new(my_order)
     my_order.add(choice_1)
     my_order.add(choice_2)
@@ -50,7 +52,7 @@ RSpec.describe Order do
     choice_4 = Menu.new("Prawn Crackers")
     choice_5 = Menu.new("Prawn Crackers")
     choice_6 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     order_message = Text.new(my_order)
     my_order.add(choice_1)
     my_order.add(choice_2)
@@ -64,7 +66,7 @@ RSpec.describe Order do
   it "returns an itemised receipt with grand total" do
     choice_1 = Menu.new("Egg Fried Rice")
     choice_2 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     order_message = Text.new(my_order)
     my_order.add(choice_1)
     my_order.add(choice_2)
@@ -75,7 +77,7 @@ RSpec.describe Order do
   it "if @order_placed is false then receipt can't be printed" do
     choice_1 = Menu.new("Egg Fried Rice")
     choice_2 = Menu.new("Prawn Crackers")
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     order_message = Text.new(my_order)
     my_order.add(choice_1)
     my_order.add(choice_2)
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,8 +1,10 @@
 require_relative "../lib/order"
+require 'dotenv'
+Dotenv.load('file1.env')
 
 RSpec.describe Order do
   it "Takes an instance of menu (item), adds it to order, reads it back" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     allow(fake_choice).to receive(:list_choice).and_return({"Egg Fried Rice" => 4.00})
     my_order.add(fake_choice)
@@ -10,33 +12,33 @@ RSpec.describe Order do
   end
   
   it "If no item is added it returns notice" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     expect(my_order.read_back).to eq "Your order is empty"
   end
   
   it "Takes an instance of menu (item), and notifies user if not on menu" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     allow(fake_choice).to receive(:list_choice).and_return("That is not on the menu")
     expect(my_order.add(fake_choice)).to eq "That is not on the menu"
   end
 
   it "Takes an instance of menu (item), and notifies user if not on menu" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     allow(fake_choice).to receive(:list_choice).and_return("That is not on the menu")
     expect(my_order.add(fake_choice)).to eq "That is not on the menu"
   end
 
   it "Place order is selected without adding item, notify user" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     expect(my_order.place_order).to eq "You have not added any items to your order"
   end
   
   it "Prints receipt with items and total" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     allow(fake_choice).to receive(:list_choice).and_return({"Egg Fried Rice" => 4.00})
     my_order.add(fake_choice)
@@ -45,7 +47,7 @@ RSpec.describe Order do
   end
   
   it "Takes an instance of menu (item), adds it to order, reads it back" do
-    my_order = Order.new("Adam", "07584573146")
+    my_order = Order.new("Adam", ENV["mob_number"])
     fake_choice = double :fake_choice
     allow(fake_choice).to receive(:list_choice).and_return({"Egg Fried Rice" => 4.00})
     my_order.add(fake_choice)
